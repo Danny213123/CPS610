47d83bf8a65935f7155287edad4dde112f9a7cc3
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2024-01-28 23:08:04 -0500

    Create insert_table.sql

---
+++ insert_table.sql
@@ -0,0 +1,49 @@
+INSERT INTO grade_report VALUES (101, 1, 'A');
+INSERT INTO grade_report VALUES (103, 2, 'B');
+INSERT INTO grade_report VALUES (104, 1, 'A');
+INSERT INTO grade_report VALUES (106, 1, 'B');
+INSERT INTO grade_report VALUES (108, 2, 'B');
+
+
+INSERT INTO grade_report VALUES (101, 3, 'B');
+INSERT INTO grade_report VALUES (103, 3, 'B');
+INSERT INTO grade_report VALUES (104, 3, 'A');
+INSERT INTO grade_report VALUES (106, 3, 'A');
+INSERT INTO grade_report VALUES (108, 3, 'C');
+
+
+INSERT INTO grade_report VALUES (101, 12, 'B');
+INSERT INTO grade_report VALUES (103, 12, 'A');
+INSERT INTO grade_report VALUES (104, 12, 'A');
+INSERT INTO grade_report VALUES (106, 12, 'B');
+INSERT INTO grade_report VALUES (108, 12, 'A');
+
+
+INSERT INTO grade_report VALUES (101, 13, 'B');
+INSERT INTO grade_report VALUES (103, 13, 'C');
+INSERT INTO grade_report VALUES (104, 13, 'A');
+INSERT INTO grade_report VALUES (106, 13, 'A');
+INSERT INTO grade_report VALUES (108, 13, 'B');
+
+
+INSERT INTO grade_report VALUES (102, 20, 'B');
+INSERT INTO grade_report VALUES (110, 20, 'A');
+
+
+INSERT INTO grade_report VALUES (102, 22, 'A');
+INSERT INTO grade_report VALUES (110, 22, 'A');
+
+
+INSERT INTO grade_report VALUES (105, 40, 'A');
+INSERT INTO grade_report VALUES (107, 40, 'B');
+INSERT INTO grade_report VALUES (109, 40, 'B');
+
+
+INSERT INTO grade_report VALUES (105, 42, 'A');
+INSERT INTO grade_report VALUES (107, 42, 'C');
+INSERT INTO grade_report VALUES (109, 42, 'B');
+
+
+INSERT INTO prerequisite VALUES ('CPS209', 'CPS109');
+INSERT INTO prerequisite VALUES ('CPS610', 'CPS510');
+INSERT INTO prerequisite VALUES ('BLG144', 'BLH143');
