233e73573efccc122438254fe0642b094231ba07
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2024-01-30 16:25:18 -0500

    Update get_connection.java

--- java/src/main/java/com/health_care/java_healthcare_database/get_connection.java
+++ java/src/main/java/com/health_care/java_healthcare_database/get_connection.java
@@ -9,21 +9,21 @@ import java.io.*;
 
 /**
  *
- * @author 666on
+ * @author Danny Guan
  */
 public class get_connection {
     // private static final String USERNAME = "";
     // private static final String PASSWORD = "";
     // private static final String CONNECTION = "oracle.scs.ryerson.ca:1521:orcl";
     
-    private static Connection conn;
+    private static Connection conn_reserve;
+
+    public get_connection(){
+    }
     
-    public get_connection (){
+    public static Connection return_connection (){
 
         String USERNAME, PASSWORD, CONNECTION;
-        String name;
-        char grade;
-        int credit;
 
         USERNAME = readEntry("Enter Database account username: ");
         PASSWORD = readEntry("Enter Database account password: ");
@@ -31,13 +31,19 @@ public class get_connection {
 
         String dbURL1 = "jdbc:oracle:thin:" + USERNAME + "/" + PASSWORD + "@" + CONNECTION;
         try {
-            conn = DriverManager.getConnection(dbURL1);
+            Connection conn = DriverManager.getConnection(dbURL1);
             if (conn != null) {
                 System.out.println("Connected with oracle.");
+
+                conn_reserve = conn;
+                
+                return conn;
             }
         } catch (SQLException error) {
             System.out.println("Issue: " + error);
+            return null;
         }
+        return null;
     }
 
     static String readEntry(String prompt) {
@@ -57,7 +63,7 @@ public class get_connection {
     }
     
     public static Connection GetConnection(){
-        return conn;
+        return conn_reserve;
     }
 
 }
